d21e7de2a8ac7d9de87cd13ed9950d31ca09b1df
skorzany <177423232+skorzany@users.noreply.github.com>
2026-04-29 16:48:15 +0200

    Add base layout and style it

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,24 @@ import './assets/styles/reset.css';
 import './assets/styles/App.css';
 
 export default function App() {
-  return <h1>Hello, world!</h1>;
+  return (
+    <>
+      <header>
+        <h1>POKÉMON memory game</h1>
+        <div className="scoreboard">
+          <h3 className="streak">Streak: 0</h3>
+          <h3 className="score">Score: 0</h3>
+        </div>
+        <div className="pokeball-outer">
+          <div className="pokeball-inner"></div>
+        </div>
+      </header>
+      <main>
+        <ul className="grid"></ul>
+      </main>
+      <footer>
+        <a href="https://github.com/skorzany/">&copy; 2026 Skorzany</a>
+      </footer>
+    </>
+  );
 }
